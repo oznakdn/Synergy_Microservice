15bc5d6bbf86c31f34c56277a6f4c6677d3a8b5b
ozan <ozanakaydin@gmail.com>
2024-02-08 14:23:29 +0300

    Shared project - update

---
+++ Shared/Synergy.Shared/Constants/RoleConstant.cs
@@ -0,0 +1,9 @@
+﻿namespace Synergy.Shared.Constants;
+
+public class RoleConstant
+{
+    public const string MANAGER = "manager";
+    public const string ADMIN = "admin";
+    public const string USER = "user";
+
+}
