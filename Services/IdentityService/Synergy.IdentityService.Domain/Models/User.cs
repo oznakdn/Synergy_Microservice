adea25c2211fe2af85080d84a3019e29c63d9021
ozan <ozanakaydin@gmail.com>
2024-02-18 10:20:18 +0300

    IdentityService - domain - update

--- Services/IdentityService/Synergy.IdentityService.Domain/Models/User.cs
+++ Services/IdentityService/Synergy.IdentityService.Domain/Models/User.cs
@@ -9,6 +9,7 @@ public class User
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
+    public string MemberId { get; set; }
     public string Username { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
