47f7c6b8e33510813e7a3b4f65af35e991252365
ozan <ozanakaydin@gmail.com>
2024-02-18 10:03:43 +0300

    IdentityService - MessageWork created - kafka message consumer

---
+++ Services/IdentityService/Synergy.IdentityService.MessageWorker/Program.cs
@@ -0,0 +1,7 @@
+using Synergy.IdentityService.MessageWorker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
