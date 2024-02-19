8e87d2804a43e8db2e2b06a232fb9200e9d7a4af
ozan <ozanakaydin@gmail.com>
2024-02-19 17:53:21 +0300

    IdentityService - update

--- Services/IdentityService/Synergy.IdentityService.Shared/Dtos/UserDtos/UserDto.cs
+++ Services/IdentityService/Synergy.IdentityService.Shared/Dtos/UserDtos/UserDto.cs
@@ -1,3 +1,3 @@
 ﻿namespace Synergy.IdentityService.Shared.Dtos.UserDtos;
 
-public record UserDto(string Id, string Username, string Email, string? Role = null);
+public record UserDto(string Id, string Username, string Email, string MemberId, string? Role = null);
