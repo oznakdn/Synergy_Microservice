4281c3d4c5c7a2a90a1f58e1f6c84376cbfc46bc
ozan <ozanakaydin@gmail.com>
2024-02-20 08:37:48 +0300

    Web project - update

--- Client/Synergy.Web/Models/AuthModels/GetProfileOutput.cs
+++ Client/Synergy.Web/Models/AuthModels/GetProfileOutput.cs
@@ -1,3 +1,3 @@
 ﻿namespace Synergy.Web.Models.AuthModels;
 
-public record GetProfileOutput(string Id, string Username, string Email, string? Role = null);
+public record GetProfileOutput(string Id, string Username, string Email, string MemberId, string? Role = null);
