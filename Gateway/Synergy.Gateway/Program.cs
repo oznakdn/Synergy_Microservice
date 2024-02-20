ac8a01afcdea1600265d9a52337cb55df5a4836a
ozan <ozanakaydin@gmail.com>
2024-02-20 12:38:11 +0300

    Gateway - Cors policy - added

--- Gateway/Synergy.Gateway/Program.cs
+++ Gateway/Synergy.Gateway/Program.cs
@@ -6,10 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOcelot();
 builder.Configuration.AddJsonFile("ocelot.json", true, true);
+builder.Services.AddCors(opt => opt.AddPolicy("Policy", policy =>
+{
+    policy.AllowAnyHeader();
+    policy.AllowAnyMethod();
+    policy.WithOrigins("http://localhost:5104");
+    policy.AllowCredentials();
+}));
 
 
 var app = builder.Build();
 
+app.UseCors("Policy");
 
 app.UseAuthorization();
 
